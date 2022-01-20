a0cf2c10b9362245c01a5b293dc2e63eb2a42972
Vlad Romanov <romvlad2002@gmail.com>
2022-01-20 16:09:55 +0300

    unfolding commit

--- info.php
+++ info.php
@@ -8,12 +8,14 @@ $info = mysqli_fetch_assoc($result1);
 
 $content1 = '';
 
+$count = 1;
+
 while($activity = mysqli_fetch_assoc($result)){
     
     
     $content1 .= '
     <div class="card">
-    <table>
+    <br id="all'.$count.'"/><table>
         <tr>
             <td>Название активности</td>
             <td>'.$activity['CourseName'].'</td>
@@ -35,9 +37,11 @@ while($activity = mysqli_fetch_assoc($result)){
             <td>'.$activity['HelpPhone'].'</td>
         </tr>
     </table>
+    <a href="#close">свернуть</a><a href="#all'.$count.'">развернуть</a>
 </div>
     ';
     
+    $count++;
 }
 
 
@@ -60,29 +64,30 @@ $content ='
             <a href="main.php"><p>На страницу поиска</p></a>
 
             <div class="card">
-    <table>
-        <tr>
-            <td width="110">Название Парка</td>
-            <td>'.$info['CommonName'].'</td>
-        </tr>
-        <tr>
-            <td>Адрес</td>
-            <td>'.$info['Location'].'</td>
-        </tr>
-        <tr>
-            <td>Время работы</td>
-            <td>'.$info['WorkingHours'].'</td>
-        </tr>
-        <tr>
-            <td>Сайт парка</td>
-            <td>'.$info['ParkWebSite'].'</td>
-        </tr>
-        <tr>
-            <td>Телефон парка</td>
-            <td>'.$info['ParkPhone'].'</td>
-        </tr>
-    </table>
-</div>
+            <br id="all0"/><table>
+                    <tr>
+                        <td width="110">Название Парка</td>
+                        <td>'.$info['CommonName'].'</td>
+                    </tr>
+                    <tr>
+                        <td>Адрес</td>
+                        <td>'.$info['Location'].'</td>
+                    </tr>
+                    <tr>
+                        <td>Время работы</td>
+                        <td>'.$info['WorkingHours'].'</td>
+                    </tr>
+                    <tr>
+                        <td>Сайт парка</td>
+                        <td>'.$info['ParkWebSite'].'</td>
+                    </tr>
+                    <tr>
+                        <td>Телефон парка</td>
+                        <td>'.$info['ParkPhone'].'</td>
+                    </tr>
+                </table>
+                <a href="#close">свернуть</a><a href="#all0">развернуть</a>
+            </div>
 
             <div style="display: flex; flex-wrap: wrap;">
                 '.$content1.'
@@ -95,36 +100,6 @@ require("template.php");
 ?>
 
 
-<!-- <div class="card">
-                        <div class="card-header">
-                            <h3>'.$activity_name.'</h3>
-                        </div>
-                        <div class="card-body p-0">
-                        <table>
-                    
-                    <tr>
-                        <td>Описание</td>
-                        <td>'.$description.'</td>
-                    </tr>
-                    <tr>
-                        <td>Время проведения</td>
-                        <td>'.$time.'</td>
-                    </tr>
-                    <tr>
-                        <td>Форма посещения</td>
-                        <td>'.$paid.'</td>
-                    </tr>
-                    <tr>
-                        <td>Телефон для справок/записи</td>
-                        <td>'.$phone.'</td>
-                    </tr>
-                </table>
-                        </div>
-                        
-                    </div>
-
-
-        </div> -->
 
 
 
